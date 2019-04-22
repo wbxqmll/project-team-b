b7e18870df3b914eb8df160454e8904ee26e908e
a0030609 <45725964+a0030609@users.noreply.github.com>
2019-04-23 00:12:27 +0800

    Update code.c
    i change the int type to unsigned int because it won't be less than 0.
    And i also add an if-else statement.

--- code.c
+++ code.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main()
 {
-   int num;
+   unsigned int num;
    printf("I like egg tarts.");
    printf("How many egg tarts have I eaten today?\n");
-   scanf("%d", &num);
+   scanf("%u", &num);
    printf("OMG! There's totally %d kcal !!!\n", num*217);
-   printf("Carson wants to eat all of them!");
+   printf("Carson wants to eat all of them!\n");
+   if(num <= 10)
+      printf("Carson is still healthy!\n");
+   else
+      printf("So bad, Carson is becoming a fat guy...\n");
    return 0;
 }
