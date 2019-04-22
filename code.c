14d3d787b664b97fd32a803a821a23d23634c376
carsonchan96 <nathniel.abc@gmail.com>
2019-04-22 16:32:50 +0800

    edit code.c

--- code.c
+++ code.c
@@ -6,5 +6,6 @@ int main()
    printf("How many egg tarts have I eaten today?\n");
    scanf("%d", &num);
    printf("OMG! There's totally %d kcal !!!\n", num*217);
+   printf("Carson wants to eat all of them!");
    return 0;
 }
