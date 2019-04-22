72d33b501820318363d58c1d1e8dfd364d8353bd
charlie2299 <49368568+charlie2299@users.noreply.github.com>
2019-04-22 22:55:07 +0800

    Update code.c

--- code.c
+++ code.c
@@ -6,7 +6,7 @@ int main()
    printf("How many egg tarts have I eaten today?\n");
    scanf("%d", &num);
    printf("OMG! There's totally %d kcal !!!\n", num*217);
-   printf("Carson wants to eat all of them!");
+   printf("Carson wants to eat all of them!\n");
    printf("There is just totally %d Cal.\n",num*217);
    printf("SO I eat %d egg tarts everyday.\n",num);
    return 0;
